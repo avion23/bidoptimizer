6a6db027700d744e7583d92683d29a385117a7e8
Ralf Waldukat <ralf.waldukat@gmail.com>
2023-07-01 10:36:05 +0200

    added more matching calculations

--- src/data_processor.py
+++ src/data_processor.py
@@ -1,17 +1,25 @@
 import pandas as pd
 import numpy as np
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 class DataProcessor:
     def __init__(self, df):
         self.df = df
 
-    def calculate_column1(self):
-        self.df['column1'] = self.df['columnA'] * self.df['columnB']  # sample calculation
+    def calculate_bid(self):
+        logger.debug("Starting bid calculation...")
+        self.df['Bid'] = self.df['Clicks'] * self.df['Spend']  # Replace with actual calculation logic
+        logger.debug("Bid calculation completed.")
 
-    def calculate_column2(self):
-        self.df['column2'] = np.sqrt(self.df['columnA'])  # sample calculation
+    def calculate_aktualitaetsindex(self):
+        logger.debug("Starting Aktualitätsindex calculation...")
+        self.df['Aktualitätsindex'] = np.sqrt(self.df['Spend'])  # Replace with actual calculation logic
+        logger.debug("Aktualitätsindex calculation completed.")
 
     def process(self):
-        self.calculate_column1()
-        self.calculate_column2()
+        self.calculate_bid()
+        self.calculate_aktualitaetsindex()
         return self.df
